ff656784c40e3b4ac49785941cc99f340ce252af
Ato Araki <ato.araki@gmail.com>
2018-08-16 23:27:20 +0900

    add microbit write daemon

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # microbitd
+
 write hex file to microbit
+
+install:
+
+    go get -u github.com/atotto/microbitd
+
+run:
+
+    microbitd
+
+You can download hex file from https://makecode.microbit.org/
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					fname := filepath.Base(event.Name)
+					if strings.HasPrefix(fname, "microbit-") && filepath.Ext(fname) == ".hex" {
+						in, err := os.Open(event.Name)
+						if err != nil {
+							log.Fatal(err)
+						}
+						out, err := os.Create("/Volumes/MICROBIT//microbit.hex")
+						if err != nil {
+							log.Println("skip:", fname)
+							break
+						}
+						if _, err := io.Copy(out, in); err != nil {
+							log.Println("write error:", fname, err)
+							break
+						}
+						out.Close()
+						log.Println("complete:", fname)
+						os.Remove(event.Name)
+					}
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	usr, _ := user.Current()
+	err = watcher.Add(filepath.Join(usr.HomeDir, "Downloads"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
